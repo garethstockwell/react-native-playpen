1c888b7110d5840c3564ea70b7d43314988a8847
Gareth Stockwell <gareth.stockwell.dev@gmail.com>
2016-03-20 01:56:57 +0000

    Add a WebView

--- src/Components/Discussion.js
+++ src/Components/Discussion.js
@@ -9,10 +9,28 @@
 import React, {
     View,
     Text,
+    WebView,
 } from 'react-native';
 
 const CommentList = require('./CommentList');
 const Styles = require('../Styles');
+
+const DUMMY_HTML = `
+<!DOCTYPE html>
+<html>
+    <body>
+        <p>
+            This is a WebView.
+            The HTML is statically compiled into the app, but the picture is an &lt;img&gt; which is fetched from DropBox.
+        </p>
+        <p>
+            <center>
+                <img src="https://www.dropbox.com/s/5ybkozhuae8iacq/IMG_20150708_224404.jpg?raw=1" height="400">
+            </center>
+        </p>
+    </body>
+</html>
+`;
 
 class Discussion extends React.Component {
     constructor(props) {
@@ -28,17 +46,16 @@ class Discussion extends React.Component {
 
         return (
             <View style={Styles.container}>
-                <View style={Styles.titleView}>
-                    <Text style={Styles.titleText}>
-                        {this.state.discussionData['Name']}
-                    </Text>
-                    <Text style={Styles.todoText}>
-                        TODO: comment loading
-                    </Text>
-                </View>
-
-                <CommentList />
+                <WebView
+                    style={{
+                        backgroundColor: '#ffffff',
+                        width: 350,
+                        flex: 1,
+                    }}
+                    source={{html: DUMMY_HTML}}
+                />
             </View>
+
         );
     }
 }
